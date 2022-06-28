67640aa34b7c9ccd3d06c21fd7fb6362c7af8aa0
yrachid <anuar.yasser@gmail.com>
2022-06-28 20:46:20 +0100

    Implements bounded range

--- src/collections.ts
+++ src/collections.ts
@@ -1,1 +1,14 @@
-export const range = <T>(limit: number, cb: (idx: number) => T) => [ ...Array(limit).keys() ].map(cb);
+export const range = <T>(limit: number, cb: (idx: number) => T) =>
+  [...Array(limit).keys()].map(cb);
+
+export type RangeBoundaries = {
+  start?: number;
+  end: number;
+};
+
+export const boundedRange = ({ start = 0, end }: RangeBoundaries): number[] => {
+  const placeholder = [...Array(Math.abs(end - start)).keys()];
+  return start < end
+    ? placeholder.map((n) => n + start).concat([end])
+    : placeholder.map((n) => -(n - start)).concat([end]);
+};
