31d41507da7b2724a88680995b394663b2598da0
yrachid <anuar.yasser@gmail.com>
2022-06-28 20:49:21 +0100

    Moves movement logic to its own module

---
+++ src/movement.ts
@@ -0,0 +1,74 @@
+import { TerminalDimensions } from "./content";
+import domQuery, { ColumnCoordinates } from "./dom-query";
+import { MemoryDump } from "./memory-dump";
+
+const KEY_CODES = {
+  UP: "ArrowUp",
+  RIGHT: "ArrowRight",
+  DOWN: "ArrowDown",
+  LEFT: "ArrowLeft",
+};
+
+export const movement = (
+  terminalDimensions: TerminalDimensions,
+  memoryDump: MemoryDump
+) => {
+  const move = {
+    [KEY_CODES.DOWN]: (coord: ColumnCoordinates) =>
+      coord.row === terminalDimensions.rowsPerBlock - 1
+        ? { row: 0 }
+        : { row: coord.row + 1 },
+    [KEY_CODES.UP]: (coord: ColumnCoordinates) =>
+      coord.row === 0
+        ? { row: terminalDimensions.rowsPerBlock - 1 }
+        : { row: coord.row - 1 },
+    [KEY_CODES.LEFT]: (coord: ColumnCoordinates) =>
+      coord.column === 0
+        ? {
+            column: terminalDimensions.columnsPerBlock - 1,
+            block: coord.block === 0 ? 1 : 0,
+          }
+        : {
+            column: coord.column - 1,
+          },
+    [KEY_CODES.RIGHT]: (coord: ColumnCoordinates) =>
+      coord.column === terminalDimensions.columnsPerBlock - 1
+        ? {
+            column: 0,
+            block: coord.block === 0 ? 1 : 0,
+          }
+        : {
+            column: coord.column + 1,
+          },
+  };
+
+  const getNextColumn = (movement: string) => {
+    const coordinates = domQuery.getActiveColumnCoordinates();
+
+    const guessBoundary = memoryDump.getGuessBoundary(
+      coordinates.contiguousIndex
+    );
+
+    if (guessBoundary !== undefined && movement === KEY_CODES.RIGHT) {
+      return domQuery.by.contiguousIndex(guessBoundary.end + 1);
+    }
+
+    if (guessBoundary !== undefined && movement === KEY_CODES.LEFT) {
+      return domQuery.by.contiguousIndex(guessBoundary.start - 1);
+    }
+
+    const nextCoordinates = { ...coordinates, ...move[movement](coordinates) };
+    return domQuery.by.columnRowAndBlock(nextCoordinates);
+  };
+
+  return function handleCursorMovement(event: KeyboardEvent) {
+    if (!domQuery.isActiveElementATerminalColumn()) {
+      domQuery.firstColumn().focus();
+      return;
+    }
+
+    if (Object.values(KEY_CODES).includes(event.key)) {
+      getNextColumn(event.key)?.focus();
+    }
+  };
+};
--- src/terminal.ts
+++ src/terminal.ts
@@ -1,43 +1,9 @@
+import { boundedRange } from "./collections";
 import { formatMemoryDump, TerminalDimensions, TerminalRow } from "./content";
 import dom from "./dom";
 import domQuery from "./dom-query";
 import { getMemoryDump, SecurityLevels } from "./memory-dump";
-
-const KEY_CODES = {
-  UP: "ArrowUp",
-  RIGHT: "ArrowRight",
-  DOWN: "ArrowDown",
-  LEFT: "ArrowLeft",
-};
-
-const move = {
-  [KEY_CODES.DOWN]: (coord: ColumnCoordinates) =>
-    coord.row === terminalDimensions.rowsPerBlock - 1
-      ? { row: 0 }
-      : { row: coord.row + 1 },
-  [KEY_CODES.UP]: (coord: ColumnCoordinates) =>
-    coord.row === 0
-      ? { row: terminalDimensions.rowsPerBlock - 1 }
-      : { row: coord.row - 1 },
-  [KEY_CODES.LEFT]: (coord: ColumnCoordinates) =>
-    coord.column === 0
-      ? {
-          column: terminalDimensions.columnsPerBlock - 1,
-          block: coord.block === 0 ? 1 : 0,
-        }
-      : {
-          column: coord.column - 1,
-        },
-  [KEY_CODES.RIGHT]: (coord: ColumnCoordinates) =>
-    coord.column === terminalDimensions.columnsPerBlock - 1
-      ? {
-          column: 0,
-          block: coord.block === 0 ? 1 : 0,
-        }
-      : {
-          column: coord.column + 1,
-        },
-};
+import { movement } from "./movement";
 
 const terminalDimensions: TerminalDimensions = {
   rowsPerBlock: 17,
@@ -81,19 +47,19 @@ const buildBlockOfRows = (rowContent: TerminalRow[], blockIndex: number) =>
             onFocus: () => {
               const guessBounds = memoryDump.getGuessBoundary(contiguousIndex);
               if (guessBounds !== undefined) {
-                for (let i = guessBounds.start; i <= guessBounds.end; i++) {
-                  const col = domQuery.by.contiguousIndex(i);
-                  col?.classList.add("active-column");
-                }
+                boundedRange(guessBounds).forEach((i) =>
+                  domQuery.by.contiguousIndex(i)?.classList.add("active-column")
+                );
               }
             },
             onBlur: () => {
               const guessBounds = memoryDump.getGuessBoundary(contiguousIndex);
               if (guessBounds !== undefined) {
-                for (let i = guessBounds.start; i <= guessBounds.end; i++) {
-                  const col = domQuery.by.contiguousIndex(i);
-                  col?.classList.remove("active-column");
-                }
+                boundedRange(guessBounds).forEach((i) =>
+                  domQuery.by
+                    .contiguousIndex(i)
+                    ?.classList.remove("active-column")
+                );
               }
             },
           });
@@ -105,9 +71,7 @@ const buildBlockOfRows = (rowContent: TerminalRow[], blockIndex: number) =>
 const firstBlockRows = buildBlockOfRows(matrix.rowsPerBlock.firstBlock, 0);
 const secondBlockRows = buildBlockOfRows(matrix.rowsPerBlock.secondBlock, 1);
 
-const terminalBlockContainer = domQuery.terminalContainer();
-
-terminalBlockContainer?.append(
+domQuery.terminalContainer()?.append(
   dom.section({
     className: "terminal-block",
     children: firstBlockRows,
@@ -118,51 +82,5 @@ terminalBlockContainer?.append(
   })
 );
 
-document.onkeydown = (event) => {
-  if (!domQuery.isActiveElementATerminalColumn()) {
-    domQuery.firstColumn().focus();
-    return;
-  }
-
-  const activeElement = document.activeElement as HTMLElement;
-  if (Object.values(KEY_CODES).includes(event.key)) {
-    getNextColumn(activeElement, event.key)?.focus();
-  }
-};
-
-export type ColumnCoordinates = {
-  row: number;
-  column: number;
-  block: number;
-  contiguousIndex: number;
-};
-
-const getNextColumn = (activeElement: HTMLElement, movement: string) => {
-  const coordinates = getColumnCoordinates(activeElement);
-
-  const guessBoundary = memoryDump.getGuessBoundary(
-    coordinates.contiguousIndex
-  );
-
-  if (guessBoundary !== undefined && movement === KEY_CODES.RIGHT) {
-    return domQuery.by.contiguousIndex(guessBoundary.end + 1);
-  }
-
-  if (guessBoundary !== undefined && movement === KEY_CODES.LEFT) {
-    return domQuery.by.contiguousIndex(guessBoundary.start - 1);
-  }
-
-  const nextCoordinates = { ...coordinates, ...move[movement](coordinates) };
-  return domQuery.by.columnRowAndBlock(nextCoordinates);
-};
-
-const getColumnCoordinates = (activeColumn: HTMLElement): ColumnCoordinates => {
-  return {
-    row: parseInt(activeColumn.dataset.row!),
-    column: parseInt(activeColumn.dataset.column!),
-    block: parseInt(activeColumn.dataset.block!),
-    contiguousIndex: parseInt(activeColumn.dataset.contiguousIndex!),
-  };
-};
-
+document.onkeydown = movement(terminalDimensions, memoryDump);
 domQuery.firstColumn().focus();
